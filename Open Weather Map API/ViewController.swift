7c6e8026a6e86256fc7a43c18c1ad2318c517c11
Andreas Velounias <Andreas.v@hotmail.co.uk>
2018-10-02 17:44:10 +0100

    Initial Commit

---
+++ Open Weather Map API/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Open Weather Map API
+//
+//  Created by Andreas Velounias on 02/10/2018.
+//  Copyright © 2018 Andreas Velounias. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
