534e15e5800904bb1ed049a6e61882e64341c974
Andreas Velounias <Andreas.v@hotmail.co.uk>
2018-10-02 18:12:40 +0100

    Setup View

--- Open Weather Map API/ViewController.swift
+++ Open Weather Map API/ViewController.swift
@@ -9,10 +9,30 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var dayLabel: UILabel!
+    @IBOutlet weak var conditionImgView: UIImageView!
+    @IBOutlet weak var conditionLabel: UILabel!
+    @IBOutlet weak var temperatureLabel: UILabel!
+    
+    let gradientLayer: CAGradientLayer = {
+        let caGradient = CAGradientLayer()
+        caGradient.frame = UIScreen.main.bounds
+        caGradient.colors = [UIColor.cyan.cgColor, UIColor.blue.cgColor]
+        caGradient.startPoint = CGPoint(x: 0.0, y: 0.0)
+        caGradient.endPoint = CGPoint(x: 0.0, y: 1.0)
+        return caGradient
+    }()
 
+    @IBAction func refreshBtnAction(_ sender: UIButton) {
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        view.layer.insertSublayer(gradientLayer, at: 0)
     }
 
     override func didReceiveMemoryWarning() {
